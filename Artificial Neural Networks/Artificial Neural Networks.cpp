ed59217dc8c1bd6f2258ef18db472084ef3c8f2d
Dawn Yue <1179348863@qq.com>
2020-06-12 14:04:53 +0800

    练习1

--- Artificial Neural Networks/Artificial Neural Networks.cpp
+++ Artificial Neural Networks/Artificial Neural Networks.cpp
@@ -5,7 +5,7 @@
 #include <dnn.hpp>
 #pragma once
 //选择需要演示的demo
-#define DEMO_METHOD		1			//0:yolov3 demo	1:openpose demo	
+#define DEMO_METHOD		0			//0:yolov3 demo	1:openpose demo	
 //参数设置
 #define YOLOV3_VIDEO		"E:\\15\\3.MP4"		
 #define OPENPOSE_VIDEO		"E:\\15\\4.mp4"
